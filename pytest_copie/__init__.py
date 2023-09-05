b516c17321d7d4a823881d37c6a269ca3f86e48b
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-09-05 19:08:08 +0200

    refactor: drop hello_world

--- pytest_copie/__init__.py
+++ pytest_copie/__init__.py
@@ -3,12 +3,3 @@
 __version__ = "0.0.1"
 __author__ = "Pierrick Rambaud"
 __email__ = "pierrick.rambaud49@gmail.com"
-
-
-def hello_world() -> str:
-    """Hello world demo method.
-
-    Returns:
-        the hello world string
-    """
-    return "hello world !"
