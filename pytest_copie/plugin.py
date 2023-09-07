90f13a46dae4a21d40cb40c060c1f3ec6b4484a4
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2023-09-07 14:21:57 +0000

    refactor: clean code

--- pytest_copie/plugin.py
+++ pytest_copie/plugin.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 from shutil import rmtree
-from typing import Any, Callable, Generator, Optional, Union
+from typing import Any, Generator, Optional, Union
 
 import pytest
 import yaml
@@ -16,25 +16,18 @@ class Result:
         self,
         exception: Union[Exception, SystemExit, None] = None,
         exit_code: Union[str, int, None] = 0,
-        _project_path: Optional[Path] = None,
-        context: dict = {},
+        project_dir: Optional[Path] = None,
+        answers: dict = {},
     ):
         """Initialize the Result object."""
         self.exception = exception
         self.exit_code = exit_code
-        self._project_path = _project_path
-        self.context = context
-
-    @property
-    def project_path(self) -> Optional[Path]:
-        """Return the project dir if no exception occurred."""
-        if self.exception is None:
-            return self._project_path
-        return None
+        self.project_dir = project_dir
+        self.answers = answers
 
     def __repr__(self) -> str:
         """Return a string representation of the result."""
-        return f"<Result {self.exception or self.project_path}>"
+        return f"<Result {self.exception or self.project_dir}>"
 
 
 class Copie:
@@ -42,80 +35,64 @@ class Copie:
 
     def __init__(
         self,
-        _default_template: Path,
-        _output_factory: Callable,
-        _config_file: Path,
-        _counter: int = 0,
+        default_template: Path,
+        test_dir: Path,
+        config_file: Path,
+        counter: int = 0,
     ):
         """Initialize the Copie object."""
-        self._default_template = _default_template
-        self._output_factory = _output_factory
-        self._config_file = _config_file
-        self._counter = _counter
+        self.default_template = default_template
+        self.test_dir = test_dir
+        self.config_file = config_file
+        self.counter = counter
 
-    @property
-    def default_template(self) -> Path:
-        """Return the path to the default template."""
-        return self._default_template
-
-    def _new_output_dir(self) -> Path:
-        """Return a new output dir based on the counter value."""
-        dirname = f"copie{self._counter:03d}"
-        self._counter += 1
-        return self._output_factory(dirname)
-
-    def copie(self, extra_context: dict = {}, template: Any = None) -> Result:
-        """Create a copier Project from the template and return the result.
+    def copy(self, extra_answers: dict = {}, template: Any = None) -> Result:
+        """Create a copier Project from the template and return the associated Result object.
 
         Args:
-            extra_context: extra context to pass to the copier
-            template: the template to use to create the project
+            extra_answers: extra answers to pass to the Copie object and overwrite the default ones
+            template: the template to use to create the project instead of the default ".".
 
         Returns:
             the Result object of the copier project generation
         """
-        exception: Union[None, SystemExit, Exception] = None
-        exit_code: Union[str, int, None] = 0
-        project_path = None
-        answers = {}
-
+        # set the template dir and the associated copier.yaml file
         template_dir = template or self.default_template
-        context_file = template_dir / "copier.yaml"
+        copier_file = template_dir / "copier.yaml"
 
-        output_dir = self._new_output_dir()
+        # create a new output_dir in the test dir based on the counter value
+        (output_dir := self.test_dir / f"copie{self.counter:03d}").mkdir()
+        self.counter += 1
 
         try:
-            # write the answers in the destination folder so they are used by the worker
-            config = yaml.safe_load(context_file.read_text())
-            context = {k: v.get("default", None) for k, v in config.items()}
-            context = {**context, **extra_context}
+            # get the answers from default and overwrite the one present in extra_answers.
+            questions = yaml.safe_load(copier_file.read_text())
+            answers = {q: a.get("default", None) for q, a in questions.items()}
+            answers = {**answers, **extra_answers}
 
             worker = run_copy(
                 src_path=str(template_dir),
                 dst_path=str(output_dir),
-                data=context,
+                data=answers,
                 unsafe=True,
-                answers_file=".copier-answers.yml",
             )
 
             # the project path will be the first child of the ouptut_dir
-            project_path = next(d for d in worker.dst_path.glob("*") if d.is_dir())
-
-            # the context is not written as we don't answer questions in the tests.
-            # So we regenerate it directly from the worker
-            answers = worker._answers_to_remember()
-            answers = {k: v for k, v in answers.items() if not k.startswith("_")}
+            w_project_dir = next(d for d in worker.dst_path.glob("*") if d.is_dir())
+            w_answers = worker._answers_to_remember()
+            w_answers = {q: a for q, a in answers.items() if not q.startswith("_")}
+            w_exception, w_exit_code = None, 0
 
         except SystemExit as e:
-            exception, exit_code = e, e.code
+            w_exception, w_exit_code, w_project_dir, w_answers = e, e.code, None, {}  # type: ignore
         except Exception as e:
-            exception, exit_code = e, -1
+            w_exception, w_exit_code, w_project_dir, w_answers = e, -1, None, {}  # type: ignore
 
-        return Result(exception, exit_code, project_path, answers)
+        return Result(w_exception, w_exit_code, w_project_dir, w_answers)
 
 
 @pytest.fixture
-def _copier_config_file(tmp_path_factory):
+def _copier_config_file(tmp_path_factory) -> Path:
     """Return a temporary copier config file."""
     # create a user from the tmp_path_factory fixture
     user_dir = tmp_path_factory.mktemp("user_dir")
@@ -125,11 +102,7 @@ def _copier_config_file(tmp_path_factory):
     (replay_dir := user_dir / "copier_replay").mkdir()
 
     # set up the configuration parameters in a config file
-    config = {
-        "copier_dir": str(copier_dir),
-        "replay_dir": str(replay_dir),
-    }
-
+    config = {"copier_dir": str(copier_dir), "replay_dir": str(replay_dir)}
     (config_file := user_dir / "config").write_text(yaml.dump(config))
 
     return config_file
@@ -137,9 +110,9 @@ def _copier_config_file(tmp_path_factory):
 
 @pytest.fixture
 def copie(request, tmp_path: Path, _copier_config_file: Path) -> Generator:
-    """Yield an instance of the Copie helper class.
+    """Yield an instance of the ``Copie`` helper class.
 
-    The class can then be used to generate a project from a template
+    The class can then be used to generate a project from a template.
 
     Args:
         request: the pytest request object
@@ -147,30 +120,28 @@ def copie(request, tmp_path: Path, _copier_config_file: Path) -> Generator:
         _copier_config_file: the temporary copier config file
 
     Returns:
-        the Copie instance
+        the ``Copie`` instance
 
     Example:
         res = copie.copie(extra_context={"project_name": "foo"})
     """
+    # extract the template directory from the pytest command parameter
     template_dir = Path(request.config.option.template)
-    (output_dir := tmp_path / "copie").mkdir()
 
-    def output_factory(dirname: str) -> Path:
-        (new_dir := output_dir / dirname).mkdir()
-        return new_dir
+    # set up a test directory in the tmp folder
+    (test_dir := tmp_path / "copie").mkdir()
 
-    yield Copie(template_dir, output_factory, _copier_config_file)
+    yield Copie(template_dir, test_dir, _copier_config_file)
 
-    # delete the files if necessary
+    # don't delete the files at the end of the test if requested
     if not request.config.option.keep_copied_projects:
-        rmtree(output_dir)
+        rmtree(test_dir)
 
 
 def pytest_addoption(parser):
     """Add option to the pytest command."""
     group = parser.getgroup("copie")
 
-    # --template option
     group.addoption(
         "--template",
         action="store",
@@ -180,7 +151,6 @@ def pytest_addoption(parser):
         type=str,
     )
 
-    # --keep-copied-projects option
     group.addoption(
         "--keep-copied-projects",
         action="store_true",
@@ -191,5 +161,5 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
-    """Force the template path to be absolute to protect ourselves from feature that changes path."""
+    """Force the template path to be absolute to protect ourselves from fixtures that changes path."""
     config.option.template = str(Path(config.option.template).resolve())
