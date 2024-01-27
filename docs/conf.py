63ee7769343e556e40a4c64ed75132046678e9ca
Rambaud Pierrick <12rambau@users.noreply.github.com>
2024-01-27 15:03:58 +0000

    bump: version 0.1.5 → 0.1.6

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ from datetime import datetime
 project = "pytest-copie"
 author = "Pierrick Rambaud"
 copyright = f"2020-{datetime.now().year}, {author}"
-release = "0.1.5"
+release = "0.1.6"
 
 # -- General configuration -----------------------------------------------------
 extensions = [
